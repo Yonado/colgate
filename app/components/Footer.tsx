a559407cccf3d96305ab4046362ac3ed53e83bbd
Yonado <yonatan.doanis@gmail.com>
2022-12-29 10:09:32 +0200

    Footer and Mobile Menu changes

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,32 +3,36 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div className='bg-[#F01A1D] flex flex-row gap-8 px-20 min-h-[25vh] items-center text-white'>
+        <div className='bg-[#F01A1D] flex flex-col md:py-0 py-8 md:flex-row gap-8 px-20 min-h-[25vh] items-center text-white max-w-full'>
             <div className="flex flex-1 flex-col text-white gap-2 pr-10">
                 <div className="logo text-3xl font-bold italic drop-shadow-2xl">Colgate.</div>
                 <div className="copyrights text-xs">© 2022 Colgate-Palmolive Company.
                     All rights reserved.</div>
             </div>
-            <div className='grid grid-cols-2 gap-[80px] md:gap-0 p-4 md:grid-cols-4  justify-between items-center w-full '>
-                <div className="grid-rows-3 grid flex-col text-xs underline underline-offset-2 gap-3">
-                    <Link href="/">Terms of Use</Link>
-                    <Link href="/">Privacy Policy</Link>
-                    <Link href="/">All Products</Link>
+            <div className='font-sans grid grid-cols-2 gap-[80px] md:gap-0 p-4 md:grid-cols-4  justify-between items-center w-full '>
+                <div className="grid-rows-3 grid flex-col text-sm  gap-3 w-full">
+                    <h6 className="no-underline font-semibold text-md pl-4">TERMS</h6>
+                    <Link className="underline underline-offset-2" href="/">Terms of Use</Link>
+                    <Link className="underline underline-offset-2" href="/">Privacy Policy</Link>
+                    <Link className="underline underline-offset-2" href="/">All Products</Link>
                 </div>
-                <div className="grid-rows-3 grid text-xs underline underline-offset-2 gap-3">
-                    <Link href="/">About Our Ads</Link>
-                    <Link href="/">Children's Privacy Policy</Link>
-                    <Link href="/">All Articles</Link>
+                <div className="grid-rows-3 grid text-sm  gap-3 w-full">
+                    <h6 className="no-underline font-semibold text-md pl-4">POLICIES</h6>
+                    <Link className="underline underline-offset-2" href="/">About Our Ads</Link>
+                    <Link className="underline underline-offset-2" href="/">Children's Privacy</Link>
+                    <Link className="underline underline-offset-2" href="/">All Articles</Link>
                 </div>
-                <div className="grid-rows-3 grid text-xs underline underline-offset-2 gap-3">
-                    <Link href="/">Coupons</Link>
-                    <Link href="/">Contact</Link>
-                    <Link href="/">Selling Points</Link>
+                <div className="grid-rows-3 grid text-sm gap-3 w-full">
+                    <h6 className="no-underline font-semibold text-md pl-4">PRODUCTS</h6>
+                    <Link className="underline underline-offset-2" href="/">Coupons</Link>
+                    <Link className="underline underline-offset-2" href="/">Contact</Link>
+                    <Link className="underline underline-offset-2" href="/">Selling Points</Link>
                 </div>
-                <div className="grid-rows-3 grid text-xs underline underline-offset-2 gap-3">
-                    <Link href="/">Our Mission</Link>
-                    <Link href="/">Colgate Professional</Link>
-                    <Link href="/">Educational Resources</Link>
+                <div className="grid-rows-3 grid text-sm  gap-3 w-full">
+                    <h6 className="no-underline font-semibold text-md pl-4">MISSION</h6>
+                    <Link className="underline underline-offset-2" href="/">Our Mission</Link>
+                    <Link className="underline underline-offset-2" href="/">Colgate Professional</Link>
+                    <Link className="underline underline-offset-2" href="/">Educational Resources</Link>
                 </div>
             </div>
             {/* <div className='flex justify-between items-center flex-row w-full '>
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,11 +1,42 @@
+'use client'
+
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
+
+const hamburgerIcon = <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10">
+    <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+</svg>
+
+const closeMenu = <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10">
+    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+</svg>
+
+
 
 const Navbar = () => {
+    const [toggle, useToggle] = useState(false)
+
     return (
         <div className='bg-[#F01A1D] min-h-[14vh] flex items-center'>
             <div className='flex flex-row gap-4 text-white items-center mx-8 w-full h-full justify-between md:justify-start'>
-                <div className="justify-self-start md:hidden">Mobile Menu</div>
+                <div className="justify-self-start md:hidden cursor-pointer" onClick={() => useToggle(!toggle)}>{hamburgerIcon}</div>
+                <div className={toggle ? "mobile-menu h-screen w-full fixed top-0 right-0 bottom-0 left-0 z-10 bg-black duration-200 ease-in" : "mobile-menu h-screen w-full fixed top-0 right-0 bottom-0 left-[-100%] z-10 bg-black ease-out duration-200"}>
+                    <div className="flex flex-row gap-4 text-white mx-8 my-6 justify-start h-full w-full">
+                        <div className={toggle && "absolute top-8 left-8 self-start md:hidden cursor-pointer transition ease-in duration-300"} onClick={() => useToggle(!toggle)}>{closeMenu}</div>
+                        <div className="mobile-links justify-self-start my-20 flex flex-col gap-8 text-xl">
+                            <Link href="/">Professionals</Link>
+                            <Link href="/">Research</Link>
+                            <Link href="/">Products</Link>
+                            <Link href="/">Contact</Link>
+                            <hr className="opacity-75 my-8"></hr>
+                            <Link href="/">Oral Health</Link>
+                            <Link href="/">Colgate's Mission</Link>
+                            <Link href="/">The World Smiles</Link>
+                        </div>
+                        <div className={toggle && "absolute top-16 right-2 font-bold text-xl italic self-start md:hidden cursor-pointer rotate-90"}>Colgate.</div>
+                        {/* <div className="mobile-menu-text flex items-center justify-end h-full w-full rotate-90 text-2xl italic font-bold">Colgate.</div> */}
+                    </div>
+                </div>
                 <div className="logo w-16 h-16 flex items-center justify-self-end md:justify-self-start"><img src="logo.png" alt="" /></div>
                 <div className="hidden font-sans links md:flex flex-row items-center gap-8 mx-8 text-xl font-light">
                     <Link href="/">Professionals</Link>
